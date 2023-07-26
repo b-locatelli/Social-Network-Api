433ea8daa9a285d9b0f8768f67ee0f46066463ae
Baily locatelli <bailylocatelli@gmail.com>
2023-07-25 18:24:38 -0700

    models are almost finished

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,31 @@
 const { Schema, model } = require('mongoose');
 
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // get: 
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+    }
+)
 
 const { Thought } = require('thought', thoughtSchema);
 module.exports = Thought;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,37 @@
 const { Schema, model } = require('mongoose');
 
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String, 
+            required: true,
+            unique: true,
+            // "valid adress"
+        },
+        thoughts: [
+            {
+                reference: "Thought"
+            }
+        ],
+        friends: [
+            {
+                reference: "User"
+            }
+        ],
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+    }
+)
 
 const { User } = require('user', userSchema);
 module.exports = User;
