0282cc880cb68ce17b88856a7ed44a533905ff56
Baily locatelli <bailylocatelli@gmail.com>
2023-07-25 18:49:05 -0700

    added the thoughRioutes and UserRoutes, need to work on controllers next

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController.js');
+
+router.route('/').get(getUser).post(createUser);
+
+router
+    .route('./:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+router
+    .route('/:userId/reactions')
+    .post(addFriend)
+    .delete(deleteFriend)
+
+module.exports = router;
