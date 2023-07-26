205d572db3765ed1b7a318c68903722b802158b8
Baily locatelli <bailylocatelli@gmail.com>
2023-07-25 19:15:21 -0700

    controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,9 +1,54 @@
 const { Thought, User } = require('../models');
 
 module.exports = {
-    getThought(req, res) {
-        Thought.find()
+  getThought(req, res) {
+      Thought.find()
+        .then((Thought) => res.json(Thought))
+        .catch((err) => res.status(500).json(err));
+    },
+
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.ThoughtId })
+        .select('-__v')
+        .then((Thought) =>
+          !Thought
+            ? res.status(404).json({ message: 'No Thought with that ID' })
+            : res.json(Thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+    createThought(req, res) {
+        Thought.create(req.body)
           .then((Thought) => res.json(Thought))
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+      },
+
+      deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.ThoughtId })
+          .then((Thought) =>
+            !Thought
+              ? res.status(404).json({ message: 'No Thought with that ID' })
+              : Student.deleteMany({ _id: { $in: Thought.students } })
+          )
+          .then(() => res.json({ message: 'Thought and students deleted!' }))
+          .catch((err) => res.status(500).json(err));
+      },
+
+      updateThought(req, res) {
+        Thought.findOneAndUpdate(
+          { _id: req.params.ThoughtId },
+          { $set: req.body },
+          { runValidators: true, new: true }
+        )
+          .then((Thought) =>
+            !Thought
+              ? res.status(404).json({ message: 'No Thought with this id!' })
+              : res.json(Thought)
+          )
           .catch((err) => res.status(500).json(err));
       },
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,9 @@
 const { Thought, User } = require('../models');
+
+module.exports = {
+    getUser(req, res) {
+        User.find()
+          .then((User) => res.json(User))
+          .catch((err) => res.status(500).json(err));
+      }
+    }
